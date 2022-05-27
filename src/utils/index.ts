4ed660e44e97ae86b90d430a49cbe3aa763f24fb
henriqueholtz <henrique_holtz@hotmail.com>
2022-05-27 05:02:24 -0300

    chore: Utils -> Add PasswordUtils with Mixin (from ts-mixer)

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,7 @@
 import EmailUtils from './EmailUtils';
+import PasswordUtils from './PasswordUtils';
+import { Mixin } from 'ts-mixer'; // https://www.npmjs.com/package/ts-mixer
 
-class Utils extends EmailUtils {}
+class Utils extends Mixin(EmailUtils, PasswordUtils) {}
 
 export default Utils;

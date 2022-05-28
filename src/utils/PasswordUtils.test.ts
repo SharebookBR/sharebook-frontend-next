7b5fb268f321fbd58e5e1a78fd999afef420fb7e
henriqueholtz <henrique_holtz@hotmail.com>
2022-05-28 07:53:28 -0300

    test: Add more invalid passwords to test in "PasswordUtils.test.ts"

--- src/utils/PasswordUtils.test.ts
+++ src/utils/PasswordUtils.test.ts
@@ -7,8 +7,10 @@ describe('Testing PasswordUtils', () => {
 		expect(result).toBeTruthy();
 	});
 	it('Password invalid', () => {
-		const passwordInvalid = '0123';
-		const result = Utils.PasswordIsValid(passwordInvalid);
-		expect(result).not.toBeTruthy();
+		const invalidPassowrds = ['', '1234', 'asdbkasdhajksdhasjkdhasdjkhasjdaksdjsa'];
+		invalidPassowrds.forEach((item) => {
+			const result = Utils.PasswordIsValid(item);
+			expect(result).not.toBeTruthy();
+		});
 	});
 });

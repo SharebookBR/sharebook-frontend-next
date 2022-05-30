ec6c0dc9413fd0a1663ad399932832bbe1445670
henriqueholtz <henrique_holtz@hotmail.com>
2022-05-30 04:47:42 -0300

    chore: Configs -> Add "viaCepUrl"

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,9 +1,11 @@
 interface ConfigType {
 	ApiUrl: string;
+	viaCepUrl: string;
 }
 
 const config: ConfigType = {
-	ApiUrl: process.env.NEXT_PUBLIC_SHAREBOOK_API ?? ''
+	ApiUrl: process.env.NEXT_PUBLIC_SHAREBOOK_API ?? '',
+	viaCepUrl: 'https://viacep.com.br/'
 };
 
 export default config;

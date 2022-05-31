6eea19318c6a51efff6841fae11265d2f8da72ed
henriqueholtz <henrique_holtz@hotmail.com>
2022-05-31 05:47:35 -0300

    chore: SharebookFooter -> using variant "body3"

--- src/components/SharebookFooter/index.tsx
+++ src/components/SharebookFooter/index.tsx
@@ -4,10 +4,10 @@ import React from 'react';
 import styles from './styles.module.scss';
 
 export function SharebookFooter() {
-  const currentYear = new Date().getFullYear();
-  return (
-    <footer className={styles.footer}>
-      <Typography>{`© 2018-${currentYear} Copyleft: sharebook.com.br`}</Typography>
-    </footer>
-  );
+	const currentYear = new Date().getFullYear();
+	return (
+		<footer className={styles.footer}>
+			<Typography variant="body3">{`© 2018-${currentYear} Copyleft: sharebook.com.br`}</Typography>
+		</footer>
+	);
 }

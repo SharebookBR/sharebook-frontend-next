e9283f6719cc2832dca11ae57df62f08485085d8
henriqueholtz <henrique_holtz@hotmail.com>
2022-06-02 04:47:15 -0300

    chore: Utils -> Improve  regex of "PhoneIsValid" + Add tests

--- src/utils/PhoneUtils.test.ts
+++ src/utils/PhoneUtils.test.ts
@@ -2,14 +2,14 @@ import Utils from '@sharebook-utils';
 
 describe('Testing PhoneUtils', () => {
 	it('Phone valid', () => {
-		const validPhones = ['(44) 99988-7766'];
+		const validPhones = ['(44) 99988-7766', '(44) 9 9988-7766'];
 		validPhones.forEach((item) => {
 			const result = Utils.PhoneIsValid(item);
 			expect(result).toBeTruthy();
 		});
 	});
 	it('Phone invalid', () => {
-		const invalidPhones = ['', '449988776', 'abcd'];
+		const invalidPhones = ['', '449988776', 'abcd', '(44) 9988-7766'];
 		invalidPhones.forEach((item) => {
 			const result = Utils.PhoneIsValid(item);
 			expect(result).not.toBeTruthy();
--- src/utils/PhoneUtils.ts
+++ src/utils/PhoneUtils.ts
@@ -1,9 +1,10 @@
 class PhoneUtils {
-	/** Return true if phone is valid!
+	/** Return true if phone is valid! Expected: "(44) 99988-7766" or "(44) 9 9988-7766"
 	 *
-	 *  Retorna true se o telefone estiver válido! */
+	 *  Retorna true se o telefone estiver válido! Esperado: "(44) 99988-7766" ou "(44) 9 9988-7766" */
 	static PhoneIsValid(phone: string): boolean {
-		return Boolean(phone.length >= 11 && phone.length <= 16 && /^\([1-9]{2}\) (?:[2-8]|9[1-9])[0-9]{3}\-[0-9]{4}$/.test(phone));
+		const regex = /^(\(?\d{2}\)?\s)(\d{1})([\s]?)(\d{4})-(\d{4})$/;
+		return Boolean(phone.length >= 11 && phone.length <= 16 && regex.test(phone));
 	}
 }
 

48e161d8aac34fda8dce6f8aa381f3d54421cda5
henriqueholtz <henrique_holtz@hotmail.com>
2022-06-02 06:40:34 -0300

    feat: Utils -> create DateUtils.DateIsValid (dd/MM/yyyy)

---
+++ src/utils/DateUtils.test.ts
@@ -0,0 +1,18 @@
+import Utils from '@sharebook-utils';
+
+describe('Testing DateUtils ("dd/MM/yyyy")', () => {
+	it('DateUtils valid dates', () => {
+		const validPostalCodes = ['01/01/2000', '31/10/2050', '06/02/2022', '28/02/2022'];
+		validPostalCodes.forEach((item) => {
+			const result = Utils.DateIsValid(item);
+			expect(result).toBeTruthy();
+		});
+	});
+	it('DateUtils invalid dates', () => {
+		const invalidPostalCodes = ['', '00/01/2000', '31/01/1899', '31/01/22', '29/02/2022', '15/15/2050'];
+		invalidPostalCodes.forEach((item) => {
+			const result = Utils.DateIsValid(item);
+			expect(result).not.toBeTruthy();
+		});
+	});
+});
---
+++ src/utils/DateUtils.ts
@@ -0,0 +1,12 @@
+class DateUtils {
+	/** Return true if date is valid! Expected: "dd/MM/yyyy" -> "31/01/2000"
+	 *
+	 *  Retorna true se a data estiver válida! Esperado: "dd/MM/yyyy" -> "31/01/2000" */
+	static DateIsValid(date: string): boolean {
+		return /(^(((0[1-9]|1[0-9]|2[0-8])[\/](0[1-9]|1[012]))|((29|30|31)[\/](0[13578]|1[02]))|((29|30)[\/](0[4,6,9]|11)))[\/](19|[2-9][0-9])\d\d$)|(^29[\/]02[\/](19|[2-9][0-9])$)/.test(
+			date
+		);
+	}
+}
+
+export default DateUtils;

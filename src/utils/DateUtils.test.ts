f4c09892ca254acd52828414cef4180d1ce04c2c
henriqueholtz <henrique_holtz@hotmail.com>
2022-06-03 07:15:21 -0300

    fix/chore: @sharebook-utils -> Fix tests of DateUtils

--- src/utils/DateUtils.test.ts
+++ src/utils/DateUtils.test.ts
@@ -1,17 +1,18 @@
+import { EnumDateTypes } from '@sharebook-enums';
 import Utils from '@sharebook-utils';
 
 describe('Testing DateUtils ("dd/MM/yyyy")', () => {
 	it('DateUtils valid dates', () => {
 		const validPostalCodes = ['01/01/2000', '31/10/2050', '06/02/2022', '28/02/2022'];
 		validPostalCodes.forEach((item) => {
-			const result = Utils.DateIsValid(item);
+			const result = Utils.DateIsValid(item, EnumDateTypes.ddMMyyyy);
 			expect(result).toBeTruthy();
 		});
 	});
 	it('DateUtils invalid dates', () => {
 		const invalidPostalCodes = ['', '00/01/2000', '31/01/1899', '31/01/22', '29/02/2022', '15/15/2050'];
 		invalidPostalCodes.forEach((item) => {
-			const result = Utils.DateIsValid(item);
+			const result = Utils.DateIsValid(item, EnumDateTypes.ddMMyyyy);
 			expect(result).not.toBeTruthy();
 		});
 	});

5fbe345c3c67b436fb14c00bc13424e1520c9246
henriqueholtz <henrique_holtz@hotmail.com>
2022-06-09 07:39:09 -0300

    test: /login -> using "GetInputFromMuiTextFieldByTestId" from "@sharebook-tests"

--- src/pages/login/index.test.tsx
+++ src/pages/login/index.test.tsx
@@ -3,7 +3,7 @@ import { act } from 'react-dom/test-utils';
 import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import LoginPage from './index.page';
 import axiosMock from 'axios';
-import { TestMuiButton, TestMuiTextField } from '@sharebook-tests';
+import { GetInputFromMuiTextFieldByTestId, TestMuiButton, TestMuiTextField } from '@sharebook-tests';
 
 const loginErrorMessage = 'Login e/ou senha inválidos! Verifique seus dados e tente novamente.';
 
@@ -24,37 +24,14 @@ describe('LoginPage ', () => {
 		TestMuiButton({ disabled: true, dataTestId: 'button-login' });
 	});
 	it('fields and button are working together', async () => {
-		const textFieldEmailEl = screen.getByTestId('input-email') as HTMLDivElement;
-		// 1. Email -> Should exists HTMLDivElement by testId: "input-email" with 2 children
-		expect(textFieldEmailEl).toBeInTheDocument();
-		expect(textFieldEmailEl.childElementCount).toBe(2);
-
-		// 2. Password -> Should exists HTMLDivElement by testId: "input-password"
-		const textFieldPasswordEl = screen.getByTestId('input-password') as HTMLDivElement;
-		expect(textFieldPasswordEl).toBeInTheDocument();
-		expect(textFieldPasswordEl.childElementCount).toBe(2);
-
 		// 3. Login -> Should exists HTMLButtonElement by testId: "button-login" and it`s disabled
 		const buttonLoginEl = screen.getByTestId('button-login') as HTMLButtonElement;
-		expect(buttonLoginEl).toBeInTheDocument();
-		expect(buttonLoginEl).toBeDisabled();
 
 		// #region 1. Email
 		{
-			const divEmailEl = textFieldEmailEl?.lastElementChild as HTMLDivElement;
-			// 1.1 Should be HTMLDivElement, exists 2 children, first is div
-			expect(divEmailEl).toBeInTheDocument();
-			expect(divEmailEl.childElementCount).toBe(2);
-
-			const inputEmailEl = divEmailEl?.firstElementChild as HTMLInputElement;
-			// 1.2 Should be HTMLInputElement type = "text"
-			expect(inputEmailEl).toBeInTheDocument();
-			expect(inputEmailEl.required).toBeTruthy();
-			expect(inputEmailEl.childElementCount).toBe(0);
-			expect(inputEmailEl.type).toBe('text');
-
 			const email = 'henrique_holtz@hotmail.com';
 			// 1.3 Should change
+			const inputEmailEl = GetInputFromMuiTextFieldByTestId({ dataTestId: 'input-email' });
 			fireEvent.change(inputEmailEl, { target: { value: email } });
 			expect(inputEmailEl.value).toBe(email);
 			expect(buttonLoginEl).toBeDisabled(); // should be disabled still
@@ -63,17 +40,7 @@ describe('LoginPage ', () => {
 
 		// #region 2. Password
 		{
-			const divPasswordEl = textFieldPasswordEl?.lastElementChild as HTMLDivElement;
-			// 2.1 Should be HTMLDivElement, exists 2 children, first is div
-			expect(divPasswordEl).toBeInTheDocument();
-			expect(divPasswordEl.childElementCount).toBe(2);
-
-			const inputPasswordEl = divPasswordEl?.firstElementChild as HTMLInputElement;
-			// 2.2 Should be HTMLInputElement type = "password"
-			expect(inputPasswordEl).toBeInTheDocument();
-			expect(inputPasswordEl.childElementCount).toBe(0);
-			expect(inputPasswordEl.required).toBeTruthy();
-			expect(inputPasswordEl.type).toBe('password');
+			const inputPasswordEl = GetInputFromMuiTextFieldByTestId({ dataTestId: 'input-password' });
 
 			const password = '123456';
 			// 2.3 Should change

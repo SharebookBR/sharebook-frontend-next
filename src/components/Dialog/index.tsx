3bcf6952cb4b79972c51142f1291ba5a7167ca7b
henriqueholtz <henrique_holtz@hotmail.com>
2022-06-14 07:26:21 -0300

    feat: @sharebook-components -> add SharebookDialog

---
+++ src/components/Dialog/index.tsx
@@ -0,0 +1,31 @@
+import { Dialog, DialogActions, DialogContent, DialogTitle } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
+import React, { ReactNode } from 'react';
+import { useWindowSize } from '@sharebook-hooks';
+
+interface ISharebookDialogProps {
+	title: ReactNode;
+	children: ReactNode;
+	open: boolean;
+	onClose?: () => void;
+	actions?: ReactNode;
+	className?: string;
+}
+
+//TODO: Validar se esse é o modelo padrão e alterar o "theme" e/ou criar um variant pra esse modelo
+
+export function SharebookDialog({ title, children, open, onClose, actions, className }: ISharebookDialogProps) {
+	const { width } = useWindowSize();
+	return (
+		<Dialog open={open} fullScreen={Boolean(width && width < 600)} className={className ?? ''}>
+			<DialogTitle>
+				{title}
+				{onClose && (
+					<CloseIcon onClick={onClose} style={{ position: 'relative', left: 'calc(100% - 24px)', top: '4px', cursor: 'pointer' }} />
+				)}
+			</DialogTitle>
+			<DialogContent dividers>{children}</DialogContent>
+			{actions && <DialogActions>{actions}</DialogActions>}
+		</Dialog>
+	);
+}

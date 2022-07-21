614dd8e81c89b1ce07a55bc636736a8cb9e1957e
henriqueholtz <henrique_holtz@hotmail.com>
2022-07-21 06:04:06 -0300

    chore: /logout -> redict to login with reload to re-render SharebookNavBar

--- src/pages/logout/index.page.tsx
+++ src/pages/logout/index.page.tsx
@@ -1,20 +1,21 @@
 import React, { useRef } from 'react';
 import { useAuthContext } from '@sharebook-hooks';
-import { useRouter } from 'next/router';
 import { useIsMounted } from 'usehooks-ts';
 
-// TODO: receive redirectUrl after logout
+/* TODO: 
+	- receive redirectUrl after logout
+	- Verify if it's possible, redirect to /login without reload (make sure about this problem: https://github.com/SharebookBR/sharebook-frontend-next/issues/99)
+*/
 
 export default function Logout() {
 	const { logout } = useAuthContext();
 	const isMounted = useIsMounted();
-	const { push } = useRouter();
 	const withLogoutRef = useRef<boolean>(false);
 
 	if (!withLogoutRef.current && isMounted()) {
 		withLogoutRef.current = true;
 		logout();
-		push('/login');
+		window.location.pathname = '/login';
 	}
 
 	return null;

66b8a64e4cb1bd4688180de511750940de704912
henriqueholtz <henrique_holtz@hotmail.com>
2022-07-23 08:32:40 -0300

    chore: @sharebook-configs -> using backend from production ("https://sharebook.com.br/api/")

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -5,7 +5,7 @@ interface ConfigType {
 }
 
 const config: ConfigType = {
-	ApiUrl: process.env.NEXT_PUBLIC_SHAREBOOK_API ?? 'https://dev.sharebook.com.br/api/',
+	ApiUrl: process.env.NEXT_PUBLIC_SHAREBOOK_API ?? 'https://sharebook.com.br/api/',
 	viaCepUrl: 'https://viacep.com.br/',
 	siteKeyGoogleRecaptcha: '6LcxaXAUAAAAAGM8zgwQvgMuykwiBPgMr0P7sNL3'
 };
